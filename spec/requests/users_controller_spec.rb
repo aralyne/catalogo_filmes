35a0d048b2e295c490447e2add62cafbe4935efe
Aralyne Gonçalves <aralynegs@gmail.com>
2020-12-15 23:41:21 -0300

    Criação da rotina de teste de delete da classe user

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,65 +1,73 @@
 require "rails_helper"
 
 RSpec.describe 'UsersController', type: :request do
-  describe "GET #index" do 
-    it 'must return 200 http status code' do
-      get '/users'
-      expect(response).to have_http_status(:ok) 
-    end 
-
-    context "When to list registered users" do
-      it 'must return a list of users' do
-        User.create(name: 'Kennya', email:'kennya@gmail.com')
-         
-        get '/users'
-  
-        expect(json_body[0]).to have_key(:name)
-        expect(json_body[0]).to have_key(:email)
-      end
-    end
-
-    context "When not to list registered users" do
-      it 'Inform the user that there are no users registered' do
-          
-        get '/users'
-
-        expect(json_body).to have_key(:message)
-        expect(json_body[:message]).to eq("Empty list")
-      end
-    end
-  end
-
-  describe "GET #show" do
-
-    it 'must return 200 http status code' do
+    describe "GET #index" do 
+        it 'must return 200 http status code' do
         get '/users'
         expect(response).to have_http_status(:ok) 
-    end 
+        end 
 
-    context "When to list an user" do
-        it 'must return an user' do
-        user = User.create(name: 'Diego', email:'diego@gmail.com')
-        
-        get "/users/#{user.id}"
+        context "When to list registered users" do
+        it 'must return a list of users' do
+            User.create(name: 'Kennya', email:'kennya@gmail.com')
+            
+            get '/users'
+    
+            expect(json_body[0]).to have_key(:name)
+            expect(json_body[0]).to have_key(:email)
+        end
+        end
 
-        expect(json_body).to have_key(:name)
+        context "When not to list registered users" do
+        it 'Inform the user that there are no users registered' do
+            
+            get '/users'
+
+            expect(json_body).to have_key(:message)
+            expect(json_body[:message]).to eq("Empty list")
+        end
         end
     end
 
+    describe "GET #show" do
 
-  end
+        it 'must return 200 http status code' do
+            get '/users'
+            expect(response).to have_http_status(:ok) 
+        end 
 
-  describe "PUT #update" do
+        context "When to list an user" do
+            it 'must return an user' do
+            user = User.create(name: 'Diego', email:'diego@gmail.com')
+            
+            get "/users/#{user.id}"
 
-  end
+            expect(json_body).to have_key(:name)
+            end
+        end
 
-  describe "DELETE #destroy" do
 
-  end
+    end
 
-  describe "PUT #create" do
+    describe "PUT #update" do
 
-end
+    end
+
+    describe "DELETE #destroy" do
+        context "When to delete an user" do
+                it 'must return an user' do
+                    user = User.create(name: 'Sandro', email:'sandro@gmail.com')
+                    
+                    get "/users/#{user.id}"
+
+                expect(json_body).to have_key(:name)
+                end
+        end
+    end
+
+    describe "PUT #create" do
+
+    end
 
   
 end
