eaf7b997dce14e60d13fdb21e4781eef3e0de6f8
Aralyne Gonçalves <aralynegs@gmail.com>
2020-12-17 23:18:44 -0300

    implementados os métodos e rotinas de testes: show, create, update, destroy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,50 @@
 class CategoriesController < ApplicationController
-    def index
-        categories = Category.all
-        if categories.empty?
-          render json: {message: "Empty list"}, status: :ok
-        else
-          render json: categories, status: :ok
-        end
+  def index
+      categories = Category.all
+      if categories.empty?
+        render json: {message: "Empty list"}, status: :ok
+      else
+        render json: categories, status: :ok
+      end
+  end  
+    #listar categoria passando ID
+  def show
+    category = Category.find(params[:id])
+    if category.nil?
+        render json: {message: "category not found"}, status: :ok
+    else
+        render json: category, status: :ok
     end
-end
+  end
+  #Criar categoria 
+  def create
+    category = Category.new(category_params)
+    if category.save
+        render json: category, status: :created
+    else
+        render json: {errors: category.errors}, status: :unprocessable_entity
+    end
+  end
+
+  #alterar categoria
+  def update
+    category = Category.find(params[:id])
+    if category.update(category_params)
+        head :no_content
+    else
+        render json: {errors: category.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    head :no_content
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end  
+  
+end     
+
+
--- spec/requests/categories_controller_spec.rb
+++ spec/requests/categories_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'CategoriesController', type: :request do
     end
 
     context "When not to list registered categories" do
-      it 'Inform the user that there are no categories registered' do
+      it 'Inform the category that there are no categories registered' do
           
         get '/categories'
 
@@ -27,4 +27,90 @@ RSpec.describe 'CategoriesController', type: :request do
       end
     end
   end
+
+  describe "GET #show" do
+
+  it 'must return 200 http status code' do
+    get '/categories'
+    expect(response).to have_http_status(:ok) 
+  end 
+
+  context 'When to list a category' do
+    it 'must return a category' do
+      category = Category.create(name: 'Romance')
+
+      get "/categories/#{category.id}"
+
+      expect(json_body).to have_key(:name)
+    end
+  end
+end
+
+describe 'POST #create' do
+  context 'when passing valid data' do
+    it 'need to return status code 201' do
+      category_params = {name: 'Romance'}
+      post '/categories', params: {category: category_params}
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'need to return the registered category' do
+      category_params = {name: 'Romance'}
+
+      post '/categories', params: {category: category_params}
+
+      expect(json_body).to have_key(:id)
+      expect(json_body).to have_key(:name)
+    end
+  end 
+
+  context 'when passing invalid data' do
+    it 'must return 422 http status code' do
+      category_params = {name: nil}
+
+      post '/categories', params: {category: category_params}
+
+      expect(json_body).to have_key(:errors)
+      expect(json_body[:errors][:name][0]).to eq("can't be blank")
+    end
+  end
+end
+
+describe 'PUT #update' do
+  context 'when passing valid data' do
+    it 'must return 204 http status code' do
+      category = Category.create(name: 'Romance')
+      category_params = {name: 'Drama'}
+
+      put "/categories/#{category.id}", params: {category: category_params}
+
+      expect(response).to have_http_status(:no_content)
+    end
+  end 
+
+  context 'when passing invalid data' do
+    it 'must return 422 http status code' do
+      category = Category.create(name: 'Romance')
+      category_params = {name: nil}
+
+      put "/categories/#{category.id}", params: {category: category_params}
+
+      expect(json_body).to have_key(:errors)
+      expect(json_body[:errors]).to have_key(:name)
+      expect(json_body[:errors][:name][0]).to eq("can't be blank")
+    end
+  end
+end
+
+describe 'DELETE #destroy' do
+  it 'must return 204 http status code' do
+    category = Category.create(name:"Romance")
+
+    delete "/categories/#{category.id}"
+
+    expect(response).to have_http_status(:no_content)
+  end
+end 
+
 end
